32e7396e9d07615890cf292dafda271b35e58898
TOLAPC\Tola <7678886@student.swin.edu.au>
2018-04-06 17:56:34 +1000

    Implement DFS

---
+++ src/robot/DFSSearch.java
@@ -0,0 +1,66 @@
+package robot;
+
+import java.util.ArrayList;
+
+
+public class DFSSearch extends SearchMethod {
+
+    public ArrayList<Location> blockedLocations;
+    public int[] boundaryXY;  // max x and y
+
+    public DFSSearch(Location startLocation, Location goalLocation, int[] boundaryXY, ArrayList<Location> blockedLocations){
+        this.code = "DFS";
+        this.longName = "Depth-First search";
+
+        this.Frontier = new Frontier();
+        this.Searched = new ArrayList<>();
+
+        this.startLocation = startLocation;
+        this.goalLocation = goalLocation;
+        this.boundaryXY = boundaryXY;
+        this.blockedLocations = blockedLocations;
+    }
+
+    public Location search(){
+        // initial state
+        addToFrontier( startLocation );
+        // search in frontier
+        while (Frontier.size() > 0){
+            //get an item off the fringe
+            Location thisLocation = popFromFrontier();
+            //System.out.println("Pop: " + thisLocation.getX() + "," + thisLocation.getY());
+
+            // check it is the goal item?
+            if ( thisLocation.equals(goalLocation) ){
+                return thisLocation;
+            }else{
+                //This isn't the goal, just explore
+                ArrayList<Location> newLocationList = thisLocation.explore( boundaryXY, blockedLocations);
+
+                //add the new list to the frontier
+                for(int i = newLocationList.size()-1; i >= 0; i--)
+                {
+                    addToFrontier(newLocationList.get(i));
+                }
+            }
+        } // end while
+        // no solution found
+        return null;
+    } // end search
+
+    public boolean addToFrontier(Location location)
+    {
+        //if this has been found before,
+        if(Searched.contains(location) || Frontier.contains(location)) {
+            //discard it
+            return false;
+        } else {
+            //else put this item on the first stack;
+            //System.out.println("Add: " + location.getX() + "," + location.getY());
+            Frontier.push(location);
+            return true;
+        }
+    }
+
+
+} // end class
--- src/robot/Main.java
+++ src/robot/Main.java
@@ -44,6 +44,8 @@ public class Main {
 
         switch (methodCode){
             case "DFS":
+                method = new DFSSearch(startLocation, goalLocation, boundaryXY, blockedLocations);
+                foundLocation = method.search();
                 break;
             case "BFS":
                 break;
@@ -55,6 +57,9 @@ public class Main {
                 System.out.println("Unknown method");
                 System.exit(1);
         }
+
+        printResult();
+        printVisualizer();
 
 
         method.reset();
@@ -103,7 +108,108 @@ public class Main {
         }
     } // end read file
 
-    
+    /* print final result */
+    private static void printResult(){
+        System.out.println( fileName + "  " + method.code + "  " + method.Searched.size());
+
+        if ( foundLocation == null ) {
+            System.out.println("No solution found");
+        }else{
+            for( Location loc : foundLocation.getTravelNodes() ){
+                System.out.print( loc.getDirectionFromParent() + "; ");
+            }
+            System.out.println();
+        }
+
+    } // end print result
+
+
+    /* print visualizer
+        @ start
+        $ goal
+        * path
+        | blocked
+    * */
+
+    private static void printVisualizer() {
+        System.out.println();
+        System.out.println("Visually Printing");
+        System.out.println(method.longName);
+
+        // print header
+        System.out.println();
+        System.out.print("\t");
+        for( int col=0; col<boundaryXY[0]; col++) {
+            System.out.printf("%4s", "----");
+        }
+
+        // print data table
+        for( int row=0; row<boundaryXY[1]; row++){
+            System.out.println();
+            System.out.print("\t");
+            for( int col=0; col<boundaryXY[0]; col++) {
+                Location temp = new Location( col, row );
+                System.out.printf("%1s", "|");
+                if ( startLocation.equals(temp) ) {
+                    System.out.printf("%3s", " @ ");
+                }else if ( goalLocation.equals(temp)) {
+                    System.out.printf("%3s", " $ ");
+                }else if ( blockedLocations.contains(temp)){
+                    System.out.printf("%3s", " # ");
+                }else if ( foundLocation.getTravelNodes().contains(temp)){
+                    System.out.printf("%3s", " o ");
+                }else {
+                    System.out.printf("%3s", "   ");
+                }
+            }//col
+            System.out.printf("%1s", "|");
+            // print separate
+            System.out.println();
+            System.out.print("\t");
+            for( int col=0; col<boundaryXY[0]; col++) {
+                System.out.printf("%4s", "----");
+            }
+        }
+        System.out.println();
+
+        System.out.println("\t @ start location");
+        System.out.println("\t $ goal location");
+        System.out.println("\t # obstruction");
+        System.out.println("\t o solution path");
+
+    } // end print visualization
+
+    private static void printVisualizerOld() {
+        System.out.println();
+        System.out.println("Visually Printing");
+        System.out.println(method.longName);
+        System.out.println("\t @ start location");
+        System.out.println("\t $ goal location");
+        System.out.println("\t # obstruction");
+        System.out.println("\t * solution path");
+
+        System.out.println();
+        for( int row=0; row<boundaryXY[1]; row++){
+            for( int col=0; col<boundaryXY[0]; col++) {
+                Location temp = new Location( col, row );
+
+                if ( startLocation.equals(temp) ) {
+                    System.out.print("\t@");
+                }else if ( goalLocation.equals(temp)) {
+                    System.out.print("\t$");
+                }else if ( blockedLocations.contains(temp)){
+                    System.out.print("\t#");
+                }else if ( foundLocation.getTravelNodes().contains(temp)){
+                    System.out.print("\t*");
+                }else {
+                    System.out.print("\t.");
+                }
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+    } // end print visualization
 
 
 } // end class
